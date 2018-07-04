f3f4b3d0761430e967f1d0fbe0f77635d048c470
Jarrett <jarrettklink1@gmail.com>
2018-07-04 16:36:45 -0400

    Porting of AvarnusPlayer.kt

--- src/net/draycia/AvarnusPlayer.kt
+++ src/net/draycia/AvarnusPlayer.kt
@@ -38,10 +38,78 @@ class AvarnusPlayer(var uuid : UUID, var spellbook : HashMap<String, SpellBook>)
     var showHealthBar : Boolean = true
 
     var resources = HashMap<String, ResourceBar>()
+    var spellBook = HashMap<String, SpellBook>()
 
+    @Transient var bossBars = HashMap<String, BossBar>()
 
+    var weapons = WeaponStorage()
 
+    fun stun(duration: Double) {
+        if(stunDuration < duration) {
+            isStunned = true
+            stunDuration = duration
+            val player = Bukkit.getPlayer(this.uuid)
+            player.setGlowing(true)
+            Main.scoreboard.getTeam("StunStatus").addPlayer(player)
+        }
+    }
 
+    fun poison(duration : Int, amplifier : Int) {
+        val player = Bukkit.getPlayer(this.uuid)
+        player.addPotionEffect(PotionEffect(PotionEffectType.POISON, duration, amplifier))
+    }
 
+    fun hideAllResourceBars() {
+        /* If bossBars.values() doesn't work, try bossBars.values, ignoring the () */
+        for(value in bossBars.values()) {
+            value.setVisible(false)
+        }
+        if(this.showHealthBar) this.bossBars.get("health").setVisible(true)
+    }
 
+    fun handleResourceRegen() {
+        /* The same applies here. Try ignoring the () */
+        for(entry in resources.entrySet()) {
+            var resourceBar = entry.getValue()
+            var regen : Double = resourceBar.regen / 4
+
+            if (regen < 0 && resourceBar.current + regen <= 0)
+                resourceBar.current = 0.0
+            else if (regen > 0 && resourceBar.current + regen >= resourceBar.cap)
+                resourceBar.current = resourceBar.cap.toDouble()
+            else
+                resourceBar.current += regen
+
+            System.out.println("Current: [" + resourceBar.current + "], Cap: [" + resourceBar.cap + "], Percentage: [" + resourceBar.current / resourceBar.cap)
+
+            this.bossBars.get(entry.getKey()).setProgress(resourceBar.current / resourceBar.cap)
+        }
+    }
+
+    fun storeWeapon(player : Player) {
+        var itemStack = player.getInventory().getItemInMainHand()
+        var itemTag = Weapon.getWeaponTag(ItemStack)
+        if(itemTag === null || itemTag.isEmpty()) return
+        if(!Main.instance.weapons.containsKey(itemTag)) return
+        var weapon = Main.instance.weapons.get(itemTag)
+        weapons.storeWeapon(weapon)
+        player.getInventory().setItemInMainHand(null)
+    }
+
+    fun getWeapon(player : Player) {
+        var itemStack = this.weapons.getWeapon().getProcessedItem()
+        player.getInventory().setItemInMainHand(itemStack)
+    }
+
+    fun nextWeapon() {
+
+    }
+
+    fun previousWeapon() {
+
+    }
+
+    fun updateWeapon() {
+
+    }
 }
